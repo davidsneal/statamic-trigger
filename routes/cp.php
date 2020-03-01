27afc0f397307b9d66b427449dc920c6a2c259d0
edalzell <erin@thedalzells.org>
2020-02-29 22:19:53 -0800

    better route grouping

--- routes/cp.php
+++ routes/cp.php
@@ -7,11 +7,13 @@ use Silentz\Anvil\Http\Controllers\Actions\DeploySiteController;
 use Silentz\Anvil\Http\Controllers\Actions\DeploymentLogController;
 
 Route::name('anvil.')->prefix('anvil')->group(function () {
+    Route::get('/', [ServerController::class, 'index'])->name('index');
     Route::get('/servers', [ServerController::class, 'index'])->name('servers');
-    Route::get('{server}/sites', [SiteController::class, 'index'])->name('sites');
-
-    Route::name('site.')->prefix('{server}/site/{site_id}')->group(function () {
-        Route::post('deploy', [DeploySiteController::class, '__invoke'])->name('deploy');
-        Route::get('deployment-log', [DeploymentLogController::class, '__invoke'])->name('deployment-log');
+    Route::prefix('server/{server}')->group(function () {
+        Route::get('sites', [SiteController::class, 'index'])->name('sites');
+        Route::name('site.')->prefix('site/{site_id}')->group(function () {
+            Route::post('deploy', [DeploySiteController::class, '__invoke'])->name('deploy');
+            Route::get('deployment-log', [DeploymentLogController::class, '__invoke'])->name('deployment-log');
+        });
     });
 });
