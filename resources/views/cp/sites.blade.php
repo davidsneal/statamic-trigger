a92d38f49d3936e85eca193e25ab897d5a97a8bd
edalzell <erin@thedalzells.org>
2020-03-04 20:35:15 -0800

    table

--- resources/views/cp/sites.blade.php
+++ resources/views/cp/sites.blade.php
@@ -4,31 +4,45 @@
 
 @section('content')
     <h2 class="mb-2 text-grey-80">Sites</h2>
-    <div class="flex flex-col p-2 text-sm bg-white rounded ">
+    <table class="text-sm text-center bg-white rounded table-fixed">
+        <thead>
+            <tr>
+                <th scope="col" class="py-2">Server</th>
+                <th scope="col" class="py-2">Site</th>
+                <th scope="col" class="py-2">Forge</th>
+                <th scope="col" class="py-2">Deployment Log</th>
+                <th scope="col" class="py-2">Action</th>
+            </tr>
+        </thead>
+        <tbody>
         @foreach ($servers as $server)
             @foreach ($server['sites'] as $site)
-            <div class="flex items-center mt-1">
-                <div class="w-1/5">
+            <tr class="items-center mt-1">
+                <td class="w-1/5">
                     <a href="http://{{ $site->name }}" target="_blank" class="hover:text-blue">{{ $server['name'] }}</a>
-                </div>
-                <div class="w-1/5">
+                </td>
+                <td class="w-1/5">
                     <a href="http://{{ $site->name }}" target="_blank" class="hover:text-blue">{{ $site->name }}</a>
-                </div>
-                <div class="w-1/5">
-                    <a href="https://forge.laravel.com/servers/{{ $site->serverId }}/sites/{{ $site->id }}" class="hover:text-blue">Visit in Forge</a>
-                </div>
-                <div class="w-1/5">
-                    <a href="{{ route('statamic.cp.anvil.site.deployment-log',['server'=>$site->serverId, 'site_id'=>$site->id]) }}" class="hover:text-blue">View Deployment Log</a>
-                </div>
-                <form class="w-1/4"
-                    action="{{ route('statamic.cp.anvil.site.deploy', ['server'=>$site->serverId, 'site_id'=>$site->id]) }}"
-                    method="POST"
-                >
-                    @csrf
-                    <button class="px-1 text-white bg-blue-500 rounded-lg py-sm" type="submit">Deploy Site</button>
-                </form>
-            </div class>
+                </td>
+                <td class="w-1/5">
+                    <a href="https://forge.laravel.com/servers/{{ $site->serverId }}/sites/{{ $site->id }}" class="hover:text-blue">Visit</a>
+                </td>
+                <td class="w-1/5">
+                    <a href="{{ route('statamic.cp.anvil.site.deployment-log',['server'=>$site->serverId, 'site_id'=>$site->id]) }}" class="hover:text-blue">View</a>
+                </td>
+                <td class="w-1/5">
+                    <form
+                        action="{{ route('statamic.cp.anvil.site.deploy', ['server'=>$site->serverId, 'site_id'=>$site->id]) }}"
+                        method="POST"
+                    >
+                        @csrf
+                        <button class="px-1 text-white bg-blue-500 rounded-lg py-sm" type="submit">Deploy</button>
+                    </form>
+                </td>
+            </div>
+            </tr>
             @endforeach
         @endforeach
-    </div>
+        </tbody>
+    </table>
 @endsection
